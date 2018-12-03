2e0017c6eb7a201afdae057f52266155167c23ae
Mervatk <39992888+Mervatk@users.noreply.github.com>
2018-12-03 02:41:01 +0200

    Done

--- Understand_Your_Vehicle/app/src/main/java/com/example/mervatkhoury/understand_your_vehicle/AppGUI.java
+++ Understand_Your_Vehicle/app/src/main/java/com/example/mervatkhoury/understand_your_vehicle/AppGUI.java
@@ -1,17 +1,20 @@
 package com.example.mervatkhoury.understand_your_vehicle;
 
 import android.content.Intent;
+import android.media.Image;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.collection.LLRBNode;
 import com.jjoe64.graphview.GraphView;
 import com.google.firebase.database.*;
 
-//Comment to check
+import com.example.mervatkhoury.understand_your_vehicle.R;
+
 public class AppGUI extends AppCompatActivity {
 
     private RTGraph graph1;
@@ -32,6 +35,13 @@ public class AppGUI extends AppCompatActivity {
     private ImageView plus2;
     private ImageView plus3;
     private ImageView plus4;
+    private ImageView setting;
+    private ImageView settings_bg1;
+    private ImageView settings_bg2;
+    private ImageView settings_ok;
+    private TextView s1;
+    private TextView s2;
+    private TextView s3;
 
     //FirebaseDatabase database = FirebaseDatabase.getInstance();
     //DatabaseReference myRef = database.getReference("message");
@@ -55,6 +65,14 @@ public class AppGUI extends AppCompatActivity {
         plus3 = (ImageView) findViewById(R.id.plus3);
         plus4 = (ImageView) findViewById(R.id.plus4);
 
+        settings_bg1 = (ImageView) findViewById(R.id.settingsBar);
+        settings_bg2 = (ImageView) findViewById(R.id.settingsBar2);
+        settings_ok = (ImageView) findViewById(R.id.ok);
+
+        s1 = (TextView) findViewById(R.id.s1);
+        s2 = (TextView) findViewById(R.id.s2);
+        s3 = (TextView) findViewById(R.id.s3);
+
         graph1 = new RTGraph(1);
         graph1.graph = (GraphView) findViewById(R.id.graph1); //what's fucking wrong??
         graph2 = new RTGraph(2);
@@ -72,6 +90,12 @@ public class AppGUI extends AppCompatActivity {
         start.setVisibility(View.VISIBLE);
         pause.setVisibility(View.GONE);
         stop.setVisibility(View.GONE);
+        settings_bg1.setVisibility(View.GONE);
+        settings_bg2.setVisibility(View.GONE);
+        settings_ok.setVisibility(View.GONE);
+        s1.setVisibility(View.GONE);
+        s2.setVisibility(View.GONE);
+        s3.setVisibility(View.GONE);
         graph1.graph.setVisibility(View.GONE);
         graph2.graph.setVisibility(View.GONE);
         graph3.graph.setVisibility(View.GONE);
@@ -140,6 +164,37 @@ public class AppGUI extends AppCompatActivity {
 
         });
 
+        setting.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                settings_bg1.setVisibility(View.VISIBLE);
+                settings_bg2.setVisibility(View.VISIBLE);
+                settings_ok.setVisibility(View.VISIBLE);
+
+                s1.setVisibility(View.VISIBLE);
+                s2.setVisibility(View.VISIBLE);
+                s3.setVisibility(View.VISIBLE);
+            }
+
+        });
+
+        settings_ok.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                settings_bg1.setVisibility(View.GONE);
+                settings_bg2.setVisibility(View.GONE);
+                settings_ok.setVisibility(View.GONE);
+
+                s1.setVisibility(View.GONE);
+                s2.setVisibility(View.GONE);
+                s3.setVisibility(View.GONE);
+
+            }
+
+        });
+
         start.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -182,6 +237,7 @@ public class AppGUI extends AppCompatActivity {
             }
 
         });
+
         plus2.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -191,6 +247,7 @@ public class AppGUI extends AppCompatActivity {
             }
 
         });
+
         plus3.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -200,6 +257,7 @@ public class AppGUI extends AppCompatActivity {
             }
 
         });
+
         plus4.setOnClickListener(new View.OnClickListener() {
 
             @Override
